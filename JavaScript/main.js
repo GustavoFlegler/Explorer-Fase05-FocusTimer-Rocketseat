7bcf99c4e073045921276e1dbb10692f56dcd66b
GustavoFlegler <rezendeflegler@gmail.com>
2023-01-12 18:42:23 -0300

    Projeto completo com Sliders de Volume

--- JavaScript/main.js
+++ JavaScript/main.js
@@ -10,10 +10,15 @@ const buttonStop = document.querySelector("#stop")
 const buttonAdd_Minutes = document.querySelector("#plus5")
 const buttonSubtract_Minutes = document.querySelector("#minus5")
 
-const buttonFlorest = document.querySelector("#florest")
-const buttonRain = document.querySelector("#rain")
-const buttonCoffee_Shop = document.querySelector("#coffee_shop")
-const buttonFireplace = document.querySelector("#fireplace")
+const buttonFlorest = document.querySelector("#florest button")
+const buttonRain = document.querySelector("#rain button")
+const buttonCoffee_Shop = document.querySelector("#coffee_shop button")
+const buttonFireplace = document.querySelector("#fireplace button")
+
+const inputFlorest = document.querySelector("#florest")
+const inputRain = document.querySelector("#rain")
+const inputCoffee_Shop = document.querySelector("#coffee_shop")
+const inputFireplace = document.querySelector("#fireplace")
 
 const mainPage = document.querySelector(".page")
 const light_Icon = document.querySelector(".light_Icon")
@@ -88,28 +93,31 @@ function togglePlay(myAudio) {
 buttonFlorest.addEventListener("click", () => {
   togglePlay(sound.florest)
   buttonFlorest.classList.toggle("themeButton_Pressed")
+  inputFlorest.classList.toggle("themeButton_Pressed")
 })
 
 buttonRain.addEventListener("click", () => {
   togglePlay(sound.rain)
   buttonRain.classList.toggle("themeButton_Pressed")
+  inputRain.classList.toggle("themeButton_Pressed")
 })
 
 buttonCoffee_Shop.addEventListener("click", () => {
   togglePlay(sound.coffee_shop)
   buttonCoffee_Shop.classList.toggle("themeButton_Pressed")
+  inputCoffee_Shop.classList.toggle("themeButton_Pressed")
 })
 
 buttonFireplace.addEventListener("click", () => {
   togglePlay(sound.fireplace)
   buttonFireplace.classList.toggle("themeButton_Pressed")
+  inputFireplace.classList.toggle("themeButton_Pressed")
 })
 
 light_Icon.addEventListener("click", () => {
   light_Icon.classList.toggle("hide")
   dark_Icon.classList.toggle("hide")
 
-  mainPage.classList.remove("lightmode")
   mainPage.classList.add("darkmode")
 })
 
@@ -118,5 +126,21 @@ dark_Icon.addEventListener("click", () => {
   dark_Icon.classList.toggle("hide")
 
   mainPage.classList.remove("darkmode")
-  mainPage.classList.add("lightmode")
+})
+
+const volumeSlider_florest = document.querySelector(".volumeSlider_florest")
+const volumeSlider_rain = document.querySelector(".volumeSlider_rain")
+const volumeSlider_coffeeShop = document.querySelector(".volumeSlider_coffeShop")
+const volumeSlider_fireplace = document.querySelector(".volumeSlider_fireplace")
+volumeSlider_rain.addEventListener("input", () => {
+  sound.rain.volume = volumeSlider_rain.value
+})
+volumeSlider_florest.addEventListener("input", () => {
+  sound.florest.volume = volumeSlider_florest.value
+})
+volumeSlider_coffeeShop.addEventListener("input", () => {
+  sound.coffee_shop.volume = volumeSlider_coffeeShop.value
+})
+volumeSlider_fireplace.addEventListener("input", () => {
+  sound.fireplace.volume = volumeSlider_fireplace.value
 })
